49c27dbd427ce401b63ab599abde40527dfe7db7
nevuos <57996991+nevuos@users.noreply.github.com>
2023-05-19 08:07:04 -0300

    feat: Add API update function (#2)
    This commit adds a new feature that enables updating the API. The update function is responsible for processing and applying available updates to the API, ensuring that the system always uses the latest version and maintains data integrity.

---
+++ api/utils/functions/check_update_function.py
@@ -0,0 +1,36 @@
+# utils/update.py
+import requests
+import subprocess
+import os
+from .internet import check_internet
+
+REPO = "user/repo"  # substitua com o usuário e nome do repositório no GitHub
+
+def check_update():
+    if not check_internet():
+        print('Sem conexão com a internet.')
+        return
+    
+    # Obtenha a versão mais recente do repositório do GitHub
+    response = requests.get(f"https://api.github.com/repos/{REPO}/releases/latest")
+    response.raise_for_status()  # Lança um erro se a solicitação falhar
+    latest_version = response.json()['tag_name']
+
+    # Abra o arquivo de versão atual
+    try:
+        with open("version.txt", "r") as file:
+            current_version = file.read().strip()
+    except FileNotFoundError:
+        current_version = None
+
+    if current_version is None or latest_version > current_version:
+        print(f"Nova versão {latest_version} disponível.")
+        update_code(latest_version)
+    else:
+        print("Está na versão mais recente.")
+
+def update_code(version):
+    subprocess.check_call(["git", "pull", "origin", "master"])  # ou a branch que você deseja
+    with open("version.txt", "w") as file:
+        file.write(version)
+    print(f"Versão atualizada para {version}.")
